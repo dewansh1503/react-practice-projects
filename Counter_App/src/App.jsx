daa048a2f4986fb55792f0dd0dbcf60510ff3601
Dewansh Pandey <dewansh1503@gmail.com>
2025-10-06 10:36:18 +0530

    adding utility functions for counter

--- Counter_App/src/App.jsx
+++ Counter_App/src/App.jsx
@@ -1,5 +1,28 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { decrease, increase } from '../features/counterSlice';
+
 function App() {
-   return <></>;
+   return (
+      <>
+         <Count />
+         {/* <Iseven /> */}
+         <Increase />
+         <Decrease />
+      </>
+   );
+}
+function Count() {
+   const count = useSelector((state) => state.count);
+   return <div>Count {count}</div>;
+}
+
+function Increase(){
+   const dispatch = useDispatch()
+   return <button onClick={()=>{dispatch(increase)}}>Increase</button>
+}
+function Decrease(){
+   const dispatch = useDispatch()
+   return <button onClick={()=>{dispatch(decrease)}}>Decrease</button>
 }
 
 export default App;
