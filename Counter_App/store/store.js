ee34ac413190c91fcb20aad7d0c2cd2d497193de
Dewansh Pandey <dewansh1503@gmail.com>
2025-10-06 21:26:31 +0530

    adding user reducer to store

--- Counter_App/store/store.js
+++ Counter_App/store/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counterSlice.js';
+import userReducer from '../features/userSlice.js';
 
 export const store = configureStore({
-   reducer: counterReducer,
+   reducer: { count: counterReducer, user: userReducer },
 });
