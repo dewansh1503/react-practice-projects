9b874e168272b6d09704b3ae2612da33ec51f4f8
Dewansh Pandey <dewansh1503@gmail.com>
2025-10-13 21:44:40 +0530

    adding routes

---
+++ Shopping_Cart/src/routes.jsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
+import Items from '../pages/Items';
+import Wishlist from '../pages/Wishlist';
+import Cart from '../pages/Cart';
+import { Tabs } from './App';
+
+const TabRoutes = () => (
+   <>
+      <Routes>
+         <Route path="/" element={<Tabs />}>
+            <Route index element={<Items />} />
+            <Route path="items" element={<Items />} />
+            <Route path="wishlist" element={<Wishlist />} />
+            <Route path="cart" element={<Cart />} />
+         </Route>
+      </Routes>
+   </>
+);
+
+export default TabRoutes;
