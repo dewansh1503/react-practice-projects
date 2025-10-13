d26907e128fbefbb2432cc97e84896ef75e4520c
Dewansh Pandey <dewansh1503@gmail.com>
2025-10-13 21:49:11 +0530

    adding reducers to store

--- Shopping_Cart/src/store.js
+++ Shopping_Cart/src/store.js
@@ -1,5 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { itemsReducer, wishlistReducer } from './slices.js';
 
-const store = configureStore({});
-
+const store = configureStore({
+   reducer: {
+      items: itemsReducer,
+      wishlist: wishlistReducer,
+   },
+});
 export default store;
