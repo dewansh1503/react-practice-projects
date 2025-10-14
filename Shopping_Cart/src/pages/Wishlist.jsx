6157d362c0e60b2f7a38e6004b4c47e99628f8f2
Dewansh Pandey <dewansh1503@gmail.com>
2025-10-14 16:02:21 +0530

    adding wishlist section

---
+++ Shopping_Cart/src/pages/Wishlist.jsx
@@ -0,0 +1,106 @@
+export default function Wishlist() {
+   const dispatch = useDispatch();
+   const wishlist = useSelector((state) => state.wishlist);
+
+   function toWishlist(item) {
+      const alreadyInWishlist = wishlist.find(
+         (product) => product.id === item.id
+      );
+      if (!alreadyInWishlist) {
+         dispatch(addToWishlist(item));
+      } else {
+         dispatch(removeFromWishlist(item.id));
+      }
+   }
+
+   return (
+      <>
+         {!wishlist.length && (
+            <div className="text-xl">No items in wishlist.</div>
+         )}
+         <ul className="list bg-base-100 rounded-box shadow-md">
+            {wishlist.map((item) => (
+               <li key={item.id} className="list-row">
+                  <div className="card card-side bg-base-100 shadow-sm">
+                     <figure>
+                        <img
+                           src={`https://picsum.photos/id/${item.imageId}/170/150`}
+                           alt="img"
+                        />
+                     </figure>
+                     <div className="card-body min-w-100 px-6 py-1">
+                        <h2 className="card-title text-2xl items-start">
+                           {item.title}
+                        </h2>
+                        <p className="text-lg">{item.description}</p>
+
+                        <div className="card-actions gap-1 items-center">
+                           <p className="text-2xl font-semibold">
+                              ${item.price}
+                           </p>
+                           <button
+                              className="btn btn-primary text-lg"
+                              onClick={() => {
+                                 toCart(item.id);
+                              }}
+                           >
+                              Cart
+                              <svg
+                                 xmlns="http://www.w3.org/2000/svg"
+                                 fill="none"
+                                 viewBox="0 0 24 24"
+                                 strokeWidth={1.5}
+                                 stroke="currentColor"
+                                 className="size-5"
+                              >
+                                 <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+                                 />
+                              </svg>
+                           </button>
+                           <button
+                              className="btn btn-square btn-ghost"
+                              onClick={() => {
+                                 toWishlist(item);
+                              }}
+                           >
+                              <svg
+                                 className="size-[1.4em]"
+                                 xmlns="http://www.w3.org/2000/svg"
+                                 viewBox="0 0 24 24"
+                                 fill={
+                                    wishlist.find(
+                                       (product) => product.id === item.id
+                                    )
+                                       ? 'red'
+                                       : 'none'
+                                 }
+                                 stroke={
+                                    wishlist.find(
+                                       (product) => product.id === item.id
+                                    )
+                                       ? 'red'
+                                       : 'currentColor'
+                                 }
+                              >
+                                 <g
+                                    strokeLinejoin="round"
+                                    strokeLinecap="round"
+                                    strokeWidth="2"
+                                 >
+                                    <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path>
+                                 </g>
+                              </svg>
+                           </button>
+                        </div>
+                     </div>
+                  </div>
+               </li>
+            ))}
+         </ul>
+         <Alert />
+      </>
+   );
+}
