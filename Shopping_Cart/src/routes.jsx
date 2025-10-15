5b358496296310b12b0a56d18bccd540dcc32e5f
Dewansh Pandey <dewansh1503@gmail.com>
2025-10-15 21:12:59 +0530

    making changes to home route

--- Shopping_Cart/src/routes.jsx
+++ Shopping_Cart/src/routes.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Items from '../pages/Items';
 import Wishlist from '../pages/Wishlist';
 import Cart from '../pages/Cart';
@@ -8,7 +8,7 @@ const TabRoutes = () => (
    <>
       <Routes>
          <Route path="/" element={<Tabs />}>
-            <Route index element={<Items />} />
+            <Route index element={<Navigate to={'items'} replace />} />
             <Route path="items" element={<Items />} />
             <Route path="wishlist" element={<Wishlist />} />
             <Route path="cart" element={<Cart />} />
