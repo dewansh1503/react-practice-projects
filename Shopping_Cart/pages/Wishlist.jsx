880851f97ee4c5977ce83f0c4d54b1008aa6a011
Dewansh Pandey <dewansh1503@gmail.com>
2025-10-24 18:23:37 +0530

    adding cart buttons to the wishlist component

--- Shopping_Cart/pages/Wishlist.jsx
+++ Shopping_Cart/pages/Wishlist.jsx
@@ -5,25 +5,23 @@ import {
    useRef,
    useState,
 } from 'react';
-import { addToWishlist, removeFromWishlist } from '../src/slices';
 import { useDispatch, useSelector } from 'react-redux';
 
 export default function Wishlist() {
    const dispatch = useDispatch();
+   const cart = useSelector((state) => state.cart);
    const wishlist = useSelector((state) => state.wishlist);
    const alertBoxRef = useRef();
 
-    function toWishlist(item) {
-      const inWishlist = wishlist.find(
-         (product) => product.id === item.id
-      );
+   function toWishlist(item) {
+      const inWishlist = wishlist.find((product) => product.id === item.id);
       if (inWishlist) {
          dispatch(removeFromWishlist(item.id));
          // console.log('removed from Wishlist');
          alertBoxRef.current?.addAlert(item);
       }
    }
-
+   
    return (
       <>
          {!wishlist.length && (
@@ -49,63 +47,89 @@ export default function Wishlist() {
                            <p className="text-2xl font-semibold">
                               ${item.price}
                            </p>
-                           <button
-                              className="btn btn-primary text-lg"
-                              onClick={() => {
-                                 toCart(item.id);
-                              }}
-                           >
-                              Cart
-                              <svg
-                                 xmlns="http://www.w3.org/2000/svg"
-                                 fill="none"
-                                 viewBox="0 0 24 24"
-                                 strokeWidth={1.5}
-                                 stroke="currentColor"
-                                 className="size-5"
-                              >
-                                 <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
-                                 />
-                              </svg>
-                           </button>
-                           <button
-                              className="btn btn-square btn-ghost"
-                              title="remove from wishlist"
-                              onClick={() => {
-                                 toWishlist(item);
-                              }}
-                           >
-                              <svg
-                                 className="size-[1.4em]"
-                                 xmlns="http://www.w3.org/2000/svg"
-                                 viewBox="0 0 24 24"
-                                 fill={
-                                    wishlist.find(
-                                       (product) => product.id === item.id
-                                    )
-                                       ? 'red'
-                                       : 'none'
-                                 }
-                                 stroke={
-                                    wishlist.find(
-                                       (product) => product.id === item.id
-                                    )
-                                       ? 'red'
-                                       : 'currentColor'
-                                 }
-                              >
-                                 <g
-                                    strokeLinejoin="round"
-                                    strokeLinecap="round"
-                                    strokeWidth="2"
+                           {isItemInCart(item.id) ? (
+                              <div className="flex items-center">
+                                 <button
+                                    className="btn btn-square font-bold text-xl w-10 h-10 "
+                                    onClick={() => {
+                                       reduceQunatity(item);
+                                    }}
                                  >
-                                    <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path>
-                                 </g>
-                              </svg>
-                           </button>
+                                    -
+                                 </button>
+                                 <span className="px-3 text-lg font-semibold ">
+                                    {getProductQuantity(item)}
+                                 </span>
+                                 <button
+                                    className="btn btn-square font-bold text-xl w-10 h-10 "
+                                    onClick={() => {
+                                       addQunatity(item);
+                                    }}
+                                 >
+                                    +
+                                 </button>
+                              </div>
+                           ) : (
+                              <div>
+                                 <button
+                                    className="btn btn-primary text-lg"
+                                    onClick={() => {
+                                       toCart(item);
+                                    }}
+                                 >
+                                    Cart
+                                    <svg
+                                       xmlns="http://www.w3.org/2000/svg"
+                                       fill="none"
+                                       viewBox="0 0 24 24"
+                                       strokeWidth={1.5}
+                                       stroke="currentColor"
+                                       className="size-5"
+                                    >
+                                       <path
+                                          strokeLinecap="round"
+                                          strokeLinejoin="round"
+                                          d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+                                       />
+                                    </svg>
+                                 </button>
+                                 <button
+                                    className="btn btn-square btn-ghost"
+                                    title="remove from wishlist"
+                                    onClick={() => {
+                                       toWishlist(item);
+                                    }}
+                                 >
+                                    <svg
+                                       className="size-[1.4em]"
+                                       xmlns="http://www.w3.org/2000/svg"
+                                       viewBox="0 0 24 24"
+                                       fill={
+                                          wishlist.find(
+                                             (product) => product.id === item.id
+                                          )
+                                             ? 'red'
+                                             : 'none'
+                                       }
+                                       stroke={
+                                          wishlist.find(
+                                             (product) => product.id === item.id
+                                          )
+                                             ? 'red'
+                                             : 'currentColor'
+                                       }
+                                    >
+                                       <g
+                                          strokeLinejoin="round"
+                                          strokeLinecap="round"
+                                          strokeWidth="2"
+                                       >
+                                          <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path>
+                                       </g>
+                                    </svg>
+                                 </button>
+                              </div>
+                           )}
                         </div>
                      </div>
                   </div>
@@ -116,6 +140,7 @@ export default function Wishlist() {
       </>
    );
 }
+
 const AlertBox = forwardRef((props, ref) => {
    const [alerts, setAlert] = useState([]);
    const containerRef = useRef(null);
@@ -157,7 +182,7 @@ const AlertBox = forwardRef((props, ref) => {
          <div
             ref={containerRef}
             style={{ scrollbarWidth: 'none', scrollBehavior: 'smooth' }}
-            className="fixed bottom-4 right-4 w-fit max-h-44 bg-transparent overflow-y-scroll "
+            className="fixed bottom-4 right-4 w-fit max-h-44 bg-transparent overflow-y-scroll z-20"
          >
             {alerts.map((item) => {
                return <Alert key={item.id} item={item} onUndo={onUndo} />;
@@ -166,7 +191,6 @@ const AlertBox = forwardRef((props, ref) => {
       </>
    );
 });
-
 
 function Alert({ item, onUndo }) {
    return (
