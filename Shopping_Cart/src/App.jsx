e26ebeddaa21a6381efb623d9c8e618534c1e32c
Dewansh Pandey <dewansh1503@gmail.com>
2025-10-27 07:49:21 +0530

    changing structure of the app

--- Shopping_Cart/src/App.jsx
+++ Shopping_Cart/src/App.jsx
@@ -17,75 +17,77 @@ export function Tabs() {
 
    return (
       <>
-         <div className="tabs tabs-border tabs-xl">
-            <Link to={'items'}>
-               <label className="tab flex items-center gap-2">
-                  <input
-                     type="radio"
-                     name="my_tabs"
-                     className="hidden"
-                     checked={currentTab === 'items'}
-                     readOnly
-                  />
-                  <span>Items</span>
-               </label>
-            </Link>
+         <div className="flex w-full box-border">
+            <div className="lg:min-w-[60%] min-w-[80%] ">
+               <div className="tabs tabs-border tabs-xl relative z-10">
+                  <Link to={'items'}>
+                     <label className="tab flex items-center gap-2">
+                        <input
+                           type="radio"
+                           name="my_tabs"
+                           className="hidden"
+                           checked={currentTab === 'items'}
+                           readOnly
+                        />
+                        <span>Items</span>
+                     </label>
+                  </Link>
 
-            <Link to={'wishlist'}>
-               <label className="tab flex items-center gap-2">
-                  <input
-                     type="radio"
-                     name="my_tabs"
-                     className="hidden"
-                     checked={currentTab === 'wishlist'}
-                     readOnly
-                  />
-                  <span>Wishlist</span>
-                  <svg
-                     className="size-4"
-                     xmlns="http://www.w3.org/2000/svg"
-                     viewBox="0 0 24 24"
-                     fill={!wishlistLength ? 'none' : 'red'}
-                     stroke={!wishlistLength ? 'currentColor' : 'red'}
-                  >
-                     <g
-                        strokeLinejoin="round"
-                        strokeLinecap="round"
-                        strokeWidth="2"
-                     >
-                        <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path>
-                     </g>
-                  </svg>
-               </label>
-            </Link>
+                  <Link to={'wishlist'}>
+                     <label className="tab flex items-center gap-2">
+                        <input
+                           type="radio"
+                           name="my_tabs"
+                           className="hidden"
+                           checked={currentTab === 'wishlist'}
+                           readOnly
+                        />
+                        <span>Wishlist</span>
+                        <svg
+                           className="size-4"
+                           xmlns="http://www.w3.org/2000/svg"
+                           viewBox="0 0 24 24"
+                           fill={!wishlistLength ? 'none' : 'red'}
+                           stroke={!wishlistLength ? 'currentColor' : 'red'}
+                        >
+                           <g
+                              strokeLinejoin="round"
+                              strokeLinecap="round"
+                              strokeWidth="2"
+                           >
+                              <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path>
+                           </g>
+                        </svg>
+                     </label>
+                  </Link>
 
-            <Link to={'cart'}>
-               <label className="tab flex items-center gap-2">
-                  <input
-                     type="radio"
-                     name="my_tabs"
-                     className="hidden"
-                     checked={currentTab === 'cart'}
-                     readOnly
-                  />
-                  <span>Cart</span>
-                  <svg
-                     xmlns="http://www.w3.org/2000/svg"
-                     viewBox="0 0 24 24"
-                     strokeWidth={1.5}
-                     fill={!cartLength ? 'none' : 'white'}
-                     stroke={'currentColor'}
-                     className="size-4"
-                  >
-                     <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
-                     />
-                  </svg>
-               </label>
-            </Link>
-         </div>
+                  <Link to={'cart'}>
+                     <label className="tab flex items-center gap-2">
+                        <input
+                           type="radio"
+                           name="my_tabs"
+                           className="hidden"
+                           checked={currentTab === 'cart'}
+                           readOnly
+                        />
+                        <span>Cart</span>
+                        <svg
+                           xmlns="http://www.w3.org/2000/svg"
+                           viewBox="0 0 24 24"
+                           strokeWidth={1.5}
+                           fill={!cart.length ? 'none' : 'white'}
+                           stroke={'currentColor'}
+                           className="size-4"
+                        >
+                           <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+                           />
+                        </svg>
+                     </label>
+                  </Link>
+               </div>
 
          <div className="border-base-300 bg-base-100 p-5">
             <Outlet />
