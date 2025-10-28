115e0e20c8a1c92df95918a1788fa94f70ad2f70
Dewansh Pandey <dewansh1503@gmail.com>
2025-10-28 20:42:41 +0530

    formatting

--- Counter_App/src/App2.jsx
+++ Counter_App/src/App2.jsx
@@ -3,46 +3,47 @@ import { fetchUser } from '../features/userSlice';
 import { useRef } from 'react';
 
 function App2() {
-   const inputRef = useRef(null);
-   const dispatch = useDispatch();
-   return (
-      <>
-         <h1>App2</h1>
-         <input type="text" ref={inputRef} />
-         <button
-            onClick={() => {
-               const userid = parseInt(inputRef.current.value);
-               if (userid > 10) alert('Enter id less than 11');
-               else if (userid && !isNaN(userid)) dispatch(fetchUser(userid));
-               else alert('enter valid id');
-            }}
-         >
-            Fetch User
-         </button>
-         <User />
-      </>
-   );
+	const inputRef = useRef(null);
+	const dispatch = useDispatch();
+	return (
+		<>
+			<h1>App2</h1>
+			<input type="text" ref={inputRef} />
+			<button
+				onClick={() => {
+					const userid = parseInt(inputRef.current.value);
+					if (userid > 10) alert('Enter id less than 11');
+					else if (userid && !isNaN(userid))
+						dispatch(fetchUser(userid));
+					else alert('enter valid id');
+				}}
+			>
+				Fetch User
+			</button>
+			<User />
+		</>
+	);
 }
 
 function User() {
-   const { loading, user, error } = useSelector((state) => state.user);
-   if (error) {
-      return <div> Failed to fetch user</div>;
-   }
-   return (
-      <>
-         {loading ? (
-            <div>Loading...</div>
-         ) : (
-            user && (
-               <div>
-                  {' '}
-                  <span>{user?.id}</span> {user?.username}
-               </div>
-            )
-         )}
-      </>
-   );
+	const { loading, user, error } = useSelector((state) => state.user);
+	if (error) {
+		return <div> Failed to fetch user</div>;
+	}
+	return (
+		<>
+			{loading ? (
+				<div>Loading...</div>
+			) : (
+				user && (
+					<div>
+						{' '}
+						<span>{user?.id}</span> {user?.username}
+					</div>
+				)
+			)}
+		</>
+	);
 }
 
 export default App2;
