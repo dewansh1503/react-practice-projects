51d50fdc5d1de2bd9ea70b3cd502f33c8391efba
Dewansh Pandey <dewansh1503@gmail.com>
2025-11-01 10:21:37 +0530

    adding the otp input box

--- OTP_Input_App/src/App.jsx
+++ OTP_Input_App/src/App.jsx
@@ -1,5 +1,25 @@
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="flex h-screen flex-col items-center justify-center gap-3">
+        <h1 className="text-2xl font-semibold">Otp box</h1>
+        <OtpInput />
+      </div>
+    </>
+  );
 }
 
+function OtpInput() {
+  const [input, setInput] = useState(Array(6).fill(""));
+  return (
+    <>
+      <div>
+        {input.map((input, i) => (
+          <InputBox key={i} setInput={setInput} value={input} index={i} />
+        ))}
+      </div>
+    </>
+  );
+}
 export default App;
