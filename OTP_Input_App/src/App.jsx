df27ec49e3f6de609c0e0e1f1785b80086446077
Dewansh Pandey <dewansh1503@gmail.com>
2025-11-01 17:14:52 +0530

    restrucutring the app

--- OTP_Input_App/src/App.jsx
+++ OTP_Input_App/src/App.jsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import { useState } from "react";
 
 function App() {
@@ -13,14 +14,47 @@ function App() {
 
 function OtpInput() {
   const [input, setInput] = useState(Array(6).fill(""));
+  const inputRef = useRef([]);
+
+  function checkBeforeInput(e) {
+    // if input is not a digit dont let the user input
+    if (/\D/.test(e.data)) e.preventDefault();
+  }
+
+  function handleChange(e, index) {
+    const newValue = e.target.value;
+    setInput((prev) =>
+      prev.map((value, i) => (i === index ? newValue : value)),
+    );
+  }
+
+  function handleKeyDown(e, i) {
+    console.log(e);
+  }
   return (
     <>
       <div>
-        {input.map((input, i) => (
-          <InputBox key={i} setInput={setInput} value={input} index={i} />
+        {input.map((value, i) => (
+          <input
+            type="text"
+            key={i}
+            inputMode="numeric"
+            maxLength={1}
+            value={value}
+            ref={(el) => (inputRef.current[i] = el)}
+            onBeforeInput={checkBeforeInput}
+            onChange={(e) => {
+              handleChange(e, i);
+            }}
+            onKeyDown={(e) => {
+              handleKeyDown(e, i);
+            }}
+            className="mx-0.5 w-7 rounded border p-2 focus:outline-none"
+          />
         ))}
       </div>
     </>
   );
 }
+
 export default App;
