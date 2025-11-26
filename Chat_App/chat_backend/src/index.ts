3ae2228c1b82d7f3aea8394ec759d6bba30cba7f
Dewansh Pandey <dewansh1503@gmail.com>
2025-11-26 13:35:32 +0530

    removed error code

--- Chat_App/chat_backend/src/index.ts
+++ Chat_App/chat_backend/src/index.ts
@@ -3,7 +3,9 @@ import { WebSocketServer } from 'ws';
 const wss = new WebSocketServer({ port: 8080 });
 
 wss.on('connection', (socket) => {
-	socket.onopen = (e) => {
-		console.log(e);
-	};
+	console.log('client connected');
+
+	socket.on('message', (e) => {
+		console.log(e.toString());
+	});
 });
