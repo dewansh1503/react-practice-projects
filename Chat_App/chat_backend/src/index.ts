7f36b63fa2b39640ee7b54eee13dd5fe20c3b8ef
Dewansh Pandey <dewansh1503@gmail.com>
2025-11-29 16:53:16 +0530

    removing log

--- Chat_App/chat_backend/src/index.ts
+++ Chat_App/chat_backend/src/index.ts
@@ -6,7 +6,7 @@ wss.on('connection', (socket) => {
 	console.log('client connected');
 
 	socket.on('message', (e) => {
-		wss.clients.forEach((c) => console.log(c.send(e.toString())));
+		wss.clients.forEach((c) => c.send(e.toString()));
 		console.log(e.toString());
 	});
 });
