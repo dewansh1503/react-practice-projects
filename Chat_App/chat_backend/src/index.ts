a4e529915337fcc338c9369834a5a85013256ecb
Dewansh Pandey <dewansh1503@gmail.com>
2025-11-30 09:22:46 +0530

    adding room logic

--- Chat_App/chat_backend/src/index.ts
+++ Chat_App/chat_backend/src/index.ts
@@ -1,12 +1,35 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer, WebSocket } from 'ws';
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', (socket) => {
-	console.log('client connected');
+type Connect = {
+	[roomId: string]: WebSocket[];
+};
+let connections: Connect = {};
 
-	socket.on('message', (e) => {
-		wss.clients.forEach((c) => c.send(e.toString()));
-		console.log(e.toString());
+interface Message {
+	type: 'join' | 'message';
+	roomId: string;
+	message?: string;
+}
+
+wss.on('connection', (socket) => {
+	// console.log('client connected');
+	socket.send('connected to server');
+	socket.on('message', (msg) => {
+		const message: Message = JSON.parse(msg.toString());
+
+		if (message.type === 'join') {
+			if (message.roomId in connections) {
+				connections[message.roomId]?.push(socket);
+			} else {
+				connections[message.roomId] = [socket];
+			}
+		} else if (message.type === 'message') {
+			connections[message.roomId]?.forEach((s) => {
+				if (message.message) s.send(message.message);
+			});
+		}
+		console.log(connections);
 	});
 });
