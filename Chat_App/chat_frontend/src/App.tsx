6d65f68f6dbe4644056adb8bbce85809d4408882
Dewansh Pandey <dewansh1503@gmail.com>
2025-11-30 09:23:21 +0530

    adding frontend for the connection

--- Chat_App/chat_frontend/src/App.tsx
+++ Chat_App/chat_frontend/src/App.tsx
@@ -1,6 +1,35 @@
+import { useRef } from 'react';
 
 function App() {
-	return <></>;
+	const soc = useRef(new WebSocket('ws://localhost:8080'));
+	const socket = soc.current;
+	socket.onmessage = (e) => {
+		console.log(e.data);
+	};
+
+	function connect() {
+		socket.send(
+			JSON.stringify({
+				roomId: '123',
+				type: 'join',
+			})
+		);
+	}
+	function send() {
+		socket.send(
+			JSON.stringify({
+				roomId: '123',
+				type: 'message',
+				message: 'message from client' + Math.random(),
+			})
+		);
+	}
+	return (
+		<>
+			<button onClick={connect}> Connect</button>
+			<button onClick={send}> Send message</button>
+		</>
+	);
 }
 
 export default App;
